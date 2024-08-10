5dd69e144f40b8f829a66720e9ce10677de1e26d
ashishdevasia <69648428+ashishdevasia@users.noreply.github.com>
2024-08-10 21:01:43 +0530

    Update run.sh

--- fan-speed-predictor/run.sh
+++ fan-speed-predictor/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
+echo "Starting Fan Speed Predictor"
+echo "Current directory: $(pwd)"
+echo "Contents of current directory:"
+ls -la
+
+echo "Running Python script"
 python3 -m uvicorn predict_fan_speed:app --host 0.0.0.0 --port 8000
